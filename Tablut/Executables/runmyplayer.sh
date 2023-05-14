788609240c734f003a8eef422deb0d68c5ed217a
Cristiano Arnaudo <108570400+skyocrandive@users.noreply.github.com>
2023-05-14 20:14:06 +0200

    Add files via upload

--- Tablut/Executables/runmyplayer.sh
+++ Tablut/Executables/runmyplayer.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+if [[ $#  -ne 3 ]] ; then
+   echo "Need all arguments"
+   echo "white or black as first parameter"
+   echo "timeout as second parameter"
+   echo "server ip as third parameter"
+   echo "Example: $0 white 60 192.168.20.254"
+   exit 1
+fi
+
 java -jar /home/tablut/tablut/RandomPlayer.jar "$1" "$2" "$3"
